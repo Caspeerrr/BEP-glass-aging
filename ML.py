20c09f3c51d7d9ef7619298f088fa47546a73047
SVisbeek <samantha.visbeek@student.uva.nl>
2021-04-17 16:17:23 +0200

    added scaling

--- ML.py
+++ ML.py
@@ -1,8 +1,13 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 def linear_regression(X, y, test_ratio):
+
+    # standardize features with z-score
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=42)
 
