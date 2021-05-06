b4da7cd73857f78e65655ad8297df5a9a56619cb
Casper Smit <caspertijmensmit@gmail.com>
2021-05-06 13:28:41 +0200

    average rdf calculator

--- avg_feature.py
+++ avg_feature.py
@@ -6,52 +6,65 @@ from ML import *
 import matplotlib.pyplot as plt
 from parameters import *
 
-iterations = 5000000
-dump_interval = 1000
 
-directory = os.fsencode(".\\traj\\")
+iterations = 50000
+dump_interval = 50
+
+rdf = True
+q6 = True
+directory = ".\\dump\\old\\"
+extension = ".OLD"
+
 amount = 0
-    
-for file in os.listdir(directory):
-     filename = os.fsdecode(file)
-     if filename.endswith(".ATOM"): 
-        timesteps, types, q6_re, q6_im, Data = read_data(".\\traj\\" + filename, iterations, dump_interval)
 
+# iterate through all dump files    
+for file in os.listdir(os.fsencode(directory)):
+     filename = os.fsdecode(file)
+     if filename.endswith(extension): 
+        
+        # retrieve and process data
+        timesteps, types, q6_re, q6_im, Data = read_data(directory + filename, iterations, dump_interval)
+        Data = {'position': Data[0], 'force': Data[1], 'q6_re': q6_re, 'q6_im': q6_im}
+
+        # declare variable for the first run
         if amount == 0:
-            tmean_q6_re, tmean_q6_im = calc_mean(q6_re), calc_mean(q6_im)
-            tvar_q6_re, tvar_q6_im = calc_variance(q6_re, tmean_q6_re), calc_variance(q6_im, tmean_q6_im)
+            
+            if rdf:
+                gr = calc_avg_rdf(Data['position'], types)
+
+            if q6:
+                mean_q6 = np.asarray((calc_mean(Data['q6_re']), calc_mean(Data['q6_im'])))
+                var_q6 = np.asarray((calc_variance(Data['q6_re'], mean_q6[0]), calc_variance(Data['q6_im'], mean_q6[1])))
 
         else:
-            mean_q6_re, mean_q6_im = calc_mean(q6_re), calc_mean(q6_im)
-            var_q6_re, var_q6_im = calc_variance(q6_re, mean_q6_re), calc_variance(q6_im, mean_q6_im)
+            if rdf:
+                gr += np.asarray((calc_avg_rdf(Data['position'], types)))
 
-            tmean_q6_re += mean_q6_re
-            tmean_q6_im += mean_q6_im
-            tvar_q6_re += var_q6_re
-            tvar_q6_im += var_q6_im
-
+            if q6:
+                mean_q6 += np.asarray((calc_mean(Data['q6_re']), calc_mean(Data['q6_im'])))
+                var_q6 += np.asarray((calc_variance(Data['q6_re'], mean_q6[0]), calc_variance(Data['q6_im'], mean_q6[1])))
         
         amount += 1
 
-tmean_q6_re /= amount
-tmean_q6_im /= amount
-tvar_q6_re /= amount
-tvar_q6_im /= amount
+# visualise the found averages
+if rdf:
+    gr /= amount
+    r = np.arange(0,rmax+dr,dr)
+    plt.plot(r, gr[0])
+    plt.title('grAA')
+    plt.xlabel('r')
+    plt.show()
+    plt.plot(r, gr[1])
+    plt.title('grBB')
+    plt.xlabel('r')
+    plt.show()
+    plt.plot(r, gr[2])
+    plt.title('grAB')
+    plt.xlabel('r')
+    plt.show()
 
-plt.plot(timesteps, tmean_q6_re)
-plt.xlabel('timesteps')
-plt.title('mean q6 real')
-plt.show()
-plt.plot(timesteps, tmean_q6_im)
-plt.xlabel('timesteps')
-plt.title('mean q6 imaginary')
-plt.show()
-plt.plot(timesteps, tvar_q6_re)
-plt.xlabel('timesteps')
-plt.title('variance q6 real')
-plt.show()
-plt.plot(timesteps, tvar_q6_im)
-plt.xlabel('timesteps')
-plt.title('variance q6 imaginary')
-plt.show()
-
+if q6:
+    mean_q6 /= amount
+    var_q6 /= amount
+    visualise(timesteps, False, q6_mean_real=mean_q6[0], q6_mean_imaginary=mean_q6[1],
+                         q6_variance_real=var_q6[0], q6_variance_imaginary=var_q6[1])
