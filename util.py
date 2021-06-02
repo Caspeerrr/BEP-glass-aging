e0ed86aa4e095e249f068362bdf84559727c16bb
Casper Smit <caspertijmensmit@gmail.com>
2021-06-02 12:05:55 +0200

    structure factor

--- util.py
+++ util.py
@@ -14,23 +14,45 @@ calc_rdf(pos, pType): radial distribution function for a single timestep
 """
 
 
+# def calc_distance(posA, posB):
+#     """
+#     calculates the distances between two particles taking the periodic boundary
+#     into consideration
+#     """
+
+#     L = 28.9
+
+#     # calculate the distance vector
+#     r1 = abs(posA - posB)
+
+#     # periodic boundary condition
+#     r2 = abs(r1 - L)
+#     r = np.amin([r1, r2], axis=0) 
+
+#     r = linalg.norm(r)
+#     return r
+
 def calc_distance(posA, posB):
     """
-    calculates the distances between two particles taking the periodic boundary
+    calculates the distance between two particles taking the periodic boundary
     into consideration
     """
 
     L = 28.9
 
-    # calculate the distance vector
-    r1 = abs(posA - posB)
+    rx = abs(posA[0] - posB[0])
+    ry = abs(posA[1] - posB[1])
 
     # periodic boundary condition
-    r2 = abs(r1 - L)
-    r = np.amin([r1, r2], axis=0) 
+    if rx > 0.5*L:
+        rx = abs(rx - L)
+    if ry > 0.5*L:
+        ry = abs(ry - L)
+    
+    r = np.sqrt(rx**2 + ry**2)
 
-    r = linalg.norm(r)
     return r
+
 
 
 def msd(posData):
@@ -80,6 +102,42 @@ def mean_nn(posData, cutoff):
                 # calculate the distance between index1 and index2
                 distance = calc_distance(pos, pos2)
 
+                if distance < cutoff:
+                    nn2[i] += 1
+                    nn2[j] += 1
+
+                nn[i] = min(nn[i], distance)
+                nn[j] = min(nn[j], distance)
+        
+
+        # calculate the mean of the nearest neighbour distance
+        nn_distance[timestep] = np.mean(nn)
+        nn_amount[timestep] = np.mean(nn2)
+        bar.next()
+    
+    bar.finish()
+    return nn_distance, nn_amount
+
+def mean_nn2(posData, cutoff):
+    """
+    calculates the mean nearest neighbour distance and the mean amount
+    of neighbours within the cutoff
+    """
+    nn_distance = np.zeros(len(posData))
+    nn_amount = np.zeros(len(posData))
+
+    bar = Bar('calc. mnn distance..', max=len(posData))
+
+    for timestep, timestepPos in enumerate(posData):
+        nn = [np.inf] * params['particles']
+        nn2 = [0] * params['particles']
+        
+
+        for i in range(params['particles']):
+            for j in range(i+1, params['particles']):
+
+                distance = calc_distance(timestepPos[i], timestepPos[j])
+            
                 if distance < cutoff:
                     nn2[i] += 1
                     nn2[j] += 1
@@ -224,6 +282,31 @@ def calc_rdf(pos, pType):
 
 
     return grAA_norm, grBB_norm, grAB_norm
+
+def calc_rdf_all(posData, types):
+    """
+    calculates the radial distribution function peak for AA, AB and BB for
+    all timesteps
+    """
+
+    grAA, grBB, grAB = np.zeros(len(posData)), np.zeros(len(posData)), np.zeros(len(posData))
+
+    bar = Bar('calc. rdf peaks..', max=len(posData))
+
+    for t, pos_t, type_t in zip(range(len(posData)), posData, types):
+
+        grAA_t, grBB_t, grAB_t = calc_rdf(pos_t, type_t)
+
+        # calculate the argmax corresponding to the first peak for grAA and grAB
+        grAA_amax[t] = np.argmax(grAA)
+        grBB_amax[t] = np.argmax(grBB)
+        grAB_amax[t] = np.argmax(grAB)
+
+        bar.next()
+
+    bar.finish()
+    
+    return grAA_amax, grBB_amax, grAB_amax
 
 def calc_rdf_peaks(posData, types):
     """
@@ -414,29 +497,134 @@ def save_load(func, savename):
     return result
 
 
-def calc_SF(rdf):
-    """
-    calculates the structure factor by taking the fourier transform of the rdf
-    @param :rdf: radial distribution function of a single timestep
-    @returns : SF_AA, SF_BB, SF_AB
-    """
-    dr = params['dr']
-    rmax = params['rmax']
-    density = params['density']
-    SF_AA, SF_BB, SF_AB = [], [], []
+def calc_SF(posData, types):
 
-    for q in np.arange(dr, rmax, dr):
-        sum_AA, sum_BB, sum_AB = 0, 0, 0
-        incre = 0
+    # initialize
+    thresh = 1e-12
+    dk = 2 * np.pi / params['Lx']
+    nmax = 100
+
+    kvec, knorm = np.zeros((nmax**2, 2)), np.zeros((nmax**2, 1))
+
+    count = 0
+
+    # calculate k vectors
+    for nx in range(0, nmax, 1):
+        for ny in range(0, nmax, 1):
+
+            kvec[count, 0] = nx * dk
+            kvec[count, 1] = ny * dk
+            knorm[count] = np.linalg.norm(kvec[count])
+            count += 1
+
+    kvec = np.delete(kvec, (0), axis=0)
+    knorm = np.delete(knorm, (0), axis=0)
+
+    Nk = kvec.shape[0]
+
+    u = np.unique(knorm)
+    ind = [0]
+    ind2 = np.nonzero(abs(np.diff(u)) >= thresh)[0]
+    ind = np.append(ind,ind2+1)
+    uk = u[np.ix_(ind)]                   # [u[i] for i in ind] 
+
+    Nuk = len(uk)
+
+    # initialize S(k)
+    SkAA = np.zeros(Nk)
+    SkBB = np.zeros(Nk)
+    skAB = np.zeros(Nk)
+
+    for i in range(Nk):
         
-        for r in np.arange(dr, rmax, dr):
-            incre += 1
-            sum_AA += r * (rdf[0][incre][0] - 1) * np.sin(q * r)
-            sum_BB += r * (rdf[1][incre][0] - 1) * np.sin(q * r)
-            sum_AB += r * (rdf[2][incre][0] - 1) * np.sin(q * r)
+        kx, ky = kvec[i, 0], kvec[i, 1]
 
-        SF_AA.append(1 + 4 * np.pi * density * sum_AA * dr / q)
-        SF_BB.append(1 + 4 * np.pi * density * sum_BB * dr / q)
-        SF_AB.append(1 + 4 * np.pi * density * sum_AB * dr / q)
+        costermA = 0
+        sintermA = 0
+        costermB = 0
+        sintermB = 0
+
+        for j, pos in enumerate(posData):
+            
+            if types[j] == 1:
+                costermA += np.cos(np.sum([kx, ky] * pos))
+                sintermA += np.sin(np.sum([kx, ky] * pos))
+            else:
+                costermB += np.cos(np.sum([kx, ky] * pos))
+                sintermB += np.sin(np.sum([kx, ky] * pos))
+
+        SkAA[i] += (costermA**2 + sintermA**2) / 650
+        SkBB[i] += (costermB**2 + sintermB**2) / 350
+
+    SkAAn, SkBBn = np.zeros(Nuk), np.zeros(Nuk)
+    for ik in range(Nuk):
+        ki   = uk[ik]
+        indk = np.nonzero(abs((knorm-ki)) < thresh)[0]
+        SkAAn[ik] = sum(SkAA[indk])/len(indk)
+        SkBBn[ik] = sum(SkBB[indk])/len(indk)
     
-    return SF_AA, SF_BB, SF_AB
+    return SkAAn, SkBBn, uk
+
+
+def rdf_poi(rdf):
+    """
+    calculates all the points of interest for the radial distribution function
+    """
+
+    features = [[], [], [], [], [], [], []]
+
+
+    for gr in rdf:
+        grAA, grBB, grAB = gr[0], gr[1], gr[2]
+
+
+        # height of the first peak of grAA
+        features[0].append(max(grAA))
+
+        # height of the second peak of grAA
+        left = round(1.5 / params['dr'])
+        right = round(2.2 / params['dr'])
+        features[1].append(max(grAA[left:right]))
+
+        # height of the fourth peak of grAA
+        left = round(2.5 / params['dr'])
+        right = round(3.5 / params['dr'])
+        features[2].append(max(grAA[left:right]))
+
+        # height of the second valley of grBB
+        left = round(1.5 / params['dr'])
+        right = round(2.5 / params['dr'])
+        features[3].append(min(grBB[left:right]))
+
+        # height of the second 'big' peak of grBB
+        left = round(2.2 / params['dr'])
+        right = round(3.3 / params['dr'])
+        features[4].append(max(grBB[left:right]))
+
+        # height of the first peak of grAB
+        features[5].append(max(grAB))
+
+        # height of the second peak of grAB
+        left = round(1.5 / params['dr'])
+        right = round(2.5 / params['dr'])
+        features[6].append(max(grAB[left:right]))
+
+    return features
+
+
+def plot_young_old(timesteps1, timesteps2, feature1, feature2):
+    """
+    plots a young and old feature side by side
+    """
+
+    ax1 = plt.subplot(121)
+    ax2 = plt.subplot(122)
+
+        
+    ax1.plot(timesteps1, feature1)
+    ax2.plot(timesteps2, feature2)
+
+    ax1.get_shared_y_axes().join(ax1, ax2)
+    ax1.set_yticklabels([])
+
+    plt.show()
